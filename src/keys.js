9f5b7c4fdf1a8225058bdc40ac48bd6750dc2d36
unknown <castillo.mejia92@gmail.com>
2024-08-11 13:01:06 -0600

    message

---
+++ src/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongodb: {
+    URI: 'mongodb+srv://User-Castillo31:VVFaXOEppZHw14dA@cluster0.13sgv.mongodb.net/UsuariosIT?retryWrites=true&w=majority&appName=Cluster0'
+  }
+};
